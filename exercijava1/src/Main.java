1bdf861471bb09454f0602020e2fa3a04a397581
F-Henrique <santos.rique89@gmail.com>
2024-08-11 20:10:20 -0300

    subindo primeiro exercicio ebac em java

---
+++ exercijava1/src/Main.java
@@ -0,0 +1,14 @@
+import classes.Estudante;
+import classes.Pessoa;
+
+public  class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+        Estudante estudante =new Estudante();
+
+        System.out.println("asd");
+
+    }
+}
